763ce6d872eb622006e3e4d8bb05169b1b9dfbbd
Candice Hall <hall.candice@outlook.com>
2022-05-07 18:50:20 -0400

    work in progress

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,13 +1,22 @@
 const router = require('express').Router();
-const { notes } = require("../../db/db.json");
+let notes = require("../../db/db.json");
 const fs = require("fs");
+const uniqid = require('uniqid');
+const path = require('path');
 
 router.get('/notes', (req, res)=>{
     res.json(notes);
 });
 
 router.post('/notes', (req, res)=>{
-console.log(req.body);
+req.body.id = uniqid();
+notes.push(req.body);
+fs.writeFileSync(
+    path.join(__dirname, "../../db/db.json"),
+    JSON.stringify(notes, null, 2)
+)
+
+
 })
 
 module.exports = router;
