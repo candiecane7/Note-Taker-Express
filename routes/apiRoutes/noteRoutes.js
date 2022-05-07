a301e095c3df4dbba54ec7376eb1c1d10b5f62da
Candice Hall <hall.candice@outlook.com>
2022-05-07 18:17:38 -0400

    working on app.post

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
 const { notes } = require("../../db/db.json");
+const fs = require("fs");
 
 router.get('/notes', (req, res)=>{
     res.json(notes);
 });
+
+router.post('/notes', (req, res)=>{
+console.log(req.body);
+})
 
 module.exports = router;
